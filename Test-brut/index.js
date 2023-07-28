e829518ccf48cfc883cfede5aaa3895b116d152f
Phanttoms <yohan.pruche@orange.fr>
2023-07-28 18:23:41 +0200

    doc-test-brut

---
+++ Test-brut/index.js
@@ -0,0 +1,47 @@
+/*
+const a = 3;
+const b = 6;
+const isMajeur = undefined;
+const notes = [6, 12, 14, 50, "yolo", [1, 2, 3]];
+
+----------------
+
+let a = "salut";
+let b = a;
+
+console.log("yolo les noobs")
+
+if (true) {
+    console.log("wesh")
+}
+
+--------------------
+
+const year = 2023;
+const birthyear = prompt('Quel est votre année de naissance ?');
+const age = year - birthyear;
+
+if (age <= 13) {
+    console.log("Le Roi Lion")
+} else if ( age <= 18) {
+    console.log("matrix")
+} else {
+    console.log("La coline a des yeux")
+}
+
+--------------------
+
+const a = prompt("Rentrez un 1er chiffre");
+const b = prompt("rentrez un 2eme chiffre");
+const result = a * b;
+
+if (result >= 0) {
+    console.log("positif")
+} else if (result < 0) {
+    console.log("negatif")
+} else {
+    console.log("operation invalide")
+}
+
+--------------------------------
+*/
