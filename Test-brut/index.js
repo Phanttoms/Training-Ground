81ecbd3c3841986eb115a4ea5555193e931fd75c
Phanttoms <yohan.pruche@orange.fr>
2023-07-28 21:50:51 +0200

    boucle

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -4,7 +4,9 @@ const b = 6;
 const isMajeur = undefined;
 const notes = [6, 12, 14, 50, "yolo", [1, 2, 3]];
 
-----------------
+//----------------
+*/
+/*
 
 let a = "salut";
 let b = a;
@@ -15,7 +17,9 @@ if (true) {
     console.log("wesh")
 }
 
---------------------
+//--------------------
+*/
+/*
 
 const year = 2023;
 const birthyear = prompt('Quel est votre année de naissance ?');
@@ -29,7 +33,9 @@ if (age <= 13) {
     console.log("La coline a des yeux")
 }
 
---------------------
+//--------------------
+*/
+/*
 
 const a = prompt("Rentrez un 1er chiffre");
 const b = prompt("rentrez un 2eme chiffre");
@@ -43,5 +49,67 @@ if (result >= 0) {
     console.log("operation invalide")
 }
 
---------------------------------
+//--------------------------------
 */
+/*
+
+const num = [4, 8, 15, 16, 23, 42];
+
+for (let i in num) {
+    console.log(num[i])
+}
+
+//----------------------------------
+*/
+/*
+
+const num = prompt("Rentrez un chiffre entre 1 et 10" );
+
+if (num > 10 || num < 1) {
+    console.log("c'est pas bon frer")
+} else {
+    for (let i = num; i >= 1; i--) {
+        console.log(i)
+    }
+}
+
+//-----------------------------------
+*/
+/*
+
+const guess = 48;
+const num = prompt("Devinez le nombre") * 1;
+
+if (num === guess) {
+    console.log("Bravo frer")
+} else if (num < guess) {
+    console.log("Plus bro")
+} else if (num > guess) {
+    console.log("Moins bro")
+} else {
+    console.log("J'ai dit un nombre !!")
+}
+
+*/
+
+/*
+// Solution avec boucle
+
+const guess = 8
+while(true) {
+    const chiffre = prompt('Votre chiffre') * 1
+    if (chiffre < guess) {
+        console.log('Plus')
+    } else if (chiffre > guess) {
+        console.log('Moins')
+    } else {
+        break
+    }
+}
+console.log('Bravo ! vous avez deviné')
+
+//---------------------------------------
+*/
+
+
+
