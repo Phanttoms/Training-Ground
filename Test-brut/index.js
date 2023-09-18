b1af2a42e38102a2e81c96eb64c91823b1ba1127
Phanttoms <yohan.pruche@orange.fr>
2023-09-18 18:21:11 +0200

    exercice

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -242,3 +242,37 @@ twiceAsOld(22,1) // 20
 twiceAsOld(29,0) // 29
 
 */
+/*
+//Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+//When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+//Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+function correct(string) {
+	const s = string.split("")
+  let result = [];
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "5") {
+      result.push("S");
+    } else if (s[i] === "0") {
+      result.push("O");
+    } else if (s[i] === "1") {
+      result.push("I");
+    } else {
+      result.push(s[i]);
+    }
+  }
+  
+  console.log(result.join(""));
+  return result.join("");
+}
+
+correct("L0ND0N")
+correct("DUBL1N")
+correct("51NGAP0RE")
+
+*/
