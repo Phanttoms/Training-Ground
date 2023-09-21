e3c92c80fa89a8a9f6015d67e2a1d748606185e1
Phanttoms <yohan.pruche@orange.fr>
2023-09-21 15:10:43 +0200

    exercice

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -391,9 +391,32 @@ findDifference([3, 2, 5], [1, 4, 4]) // 14
 
 */
 /*
+
 function yolo(x) {
   console.log([...x].sort((a, b) => a - b));
 }
 
 yolo([2, 1, 12, 4, 6, 3, 22, 18])
+
+*/
+/*
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+function mango(q, p){
+  let free = q / 3;
+  console.log((q - Math.floor(free)) * p)
+  
+}
+
+mango(3, 3) // 6
+mango(9, 5) // 30
+mango(2, 3) // 6
+mango(5, 3) // 12
+mango(38, 32) // 832
+
+// mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+// mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+// mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+
 */
