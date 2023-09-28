f4b3e7294dcf4967eec69f06a3a4b7c68a97bdb6
Phanttoms <yohan.pruche@orange.fr>
2023-09-28 11:58:31 +0200

    exercice

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -513,3 +513,39 @@ saleHotdogs(  9) //  855
 saleHotdogs( 10) //  900
 
 */
+/*
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+
+function expressionMatter(a, b, c) {
+  const op1 = a * (b + c);
+  const op2 = a * b * c;
+  const op3 = a + b * c;
+  const op4 = (a + b) * c;
+  const op5 = a + b + c;
+  let result = 0;
+
+  for (let i = 0; i < 5; i++) {
+    if (result < op1) {
+      result = op1;
+    } else if (result < op2) {
+      result = op2;
+    } else if (result < op3) {
+      result = op3;
+    } else if (result < op4) {
+      result = op4;
+    } else if (result < op5) {
+      result = op5;
+    }
+  }
+
+  
+  console.log(result);
+  return result;
+}
+
+expressionMatter(2, 1, 2) // 6
+expressionMatter(2, 1, 1) // 4
+expressionMatter(1, 1, 1) // 3
+expressionMatter(1, 2, 3) // 9
+
+*/
