471381328b74be8ccc1f1b82c8ee8bb12d6457b1
Phanttoms <yohan.pruche@orange.fr>
2023-10-03 19:00:43 +0200

    exercice

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -114,3 +114,39 @@ function moveZeros(arr) {
 moveZeros([1,2,0,1,0,1,0,3,0,1]) // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
 
 */
+/*
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+function pigIt(str){
+  str = str.trim().split(/\s{1,}/);
+  result = str.map(val => {
+    if (/^[A-Za-z]+$/.test(val)) {
+      return `${val.slice(1)}${val.slice(0, 1)}ay`;
+    }
+      return val;
+    }).join(' ');
+  
+  console.log(result)
+}
+
+pigIt('Pig latin is cool') // 'igPay atinlay siay oolcay'
+//pigIt('This is my string') // 'hisTay siay ymay tringsay'
+//pigIt('Pig latin is cool') // 'igPay atinlay siay oolcay'
+//pigIt('Hello world !');     // 'elloHay orldway !'
+
+*/
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+function humanReadable (seconds) {
+  const hour = Math.floor(seconds / 3600);
+  const minutes = seconds / 1440
+
+  console.log(hour, minutes, seconds)
+}
+
+humanReadable(0) // '00:00:00'
+humanReadable(59) // '00:00:59'
+humanReadable(60) // '00:01:00'
+humanReadable(3599) // '00:59:59'
+humanReadable(86399) // '23:59:59'
