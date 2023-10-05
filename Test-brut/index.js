a8075ff95ab2955fc7d9816ceb002a5e29ec45d8
Phanttoms <yohan.pruche@orange.fr>
2023-10-05 20:24:26 +0200

    exercice

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -172,3 +172,26 @@ humanReadable(3599) // '00:59:59'
 humanReadable(86399) // '23:59:59'
 
 */
+/*
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+function spinWords(string){
+  const ar = string.split(" ");
+  const result = [];
+  for (const element of ar) {
+    if (element.length >= 5) {
+      result.push(element.split("").reverse().join(""));
+    } else {
+      result.push(element);
+    }
+  }
+  console.log(result.join(" "))
+  return result.join(" ");
+}
+
+spinWords("Welcome") // "emocleW"
+spinWords("Hey fellow warriors") // "Hey wollef sroirraw"
+spinWords("This is a test") // "This is a test"
+spinWords("Just kidding there is still one more") // "Just gniddik ereht is llits one more"
+
+*/
