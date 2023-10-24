44441576d2fa7a3d1ee62ce406b7ff6137f66ed9
Phanttoms <yohan.pruche@orange.fr>
2023-10-24 21:52:23 +0200

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -373,3 +373,30 @@ arrayDiff([1, 2, 2], []); // [1, 2, 2];
 arrayDiff([1, 2, 2], [2]); // [1];
 
 */
+/*
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers) {
+	let result = [];
+
+	for (let i = 0; i < numbers.length; i++) {
+		if (i === 0) {
+			result.push(`(${numbers[i]}`);
+		} else if (i === 2) {
+			result.push(`${numbers[i]}) `);
+		} else if (i === 5) {
+			result.push(`${numbers[i]}-`);
+		} else {
+			result.push(`${numbers[i]}`);
+		}
+	}
+
+	console.log(result.join(""));
+	return result.join("");
+}
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); //  "(123) 456-7890"
+createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]); // "(111) 111-1111"
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // "(123) 456-7890"
+
+*/
