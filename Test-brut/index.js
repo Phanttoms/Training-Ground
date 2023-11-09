96ba4eea9491d30b305499e0cb66938141e521fd
Phanttoms <yohan.pruche@orange.fr>
2023-11-09 22:58:49 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -425,3 +425,47 @@ countBits(10); // 2
 countBits(1496656163); // 14
 
 */
+/*
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+	let result = [];
+
+	for (let i = 0; i < l.length; i++) {
+		if (l[i] === l[i] * 1) {
+			result.push(l[i]);
+		}
+	}
+
+	console.log(result);
+	return result;
+}
+
+filter_list([1, 2, "a", "b"]); // [1, 2];
+filter_list([1, "a", "b", 0, 15]); // [1, 0, 15];
+filter_list([1, 2, "aasf", "1", "123", 123]); // [1, 2, 123];
+
+*/
+/*
+// Given an integral number, determine if it's a square number:
+
+var isSquare = function (n) {
+	let result = Math.sqrt(n);
+
+	if (result === Math.floor(result)) {
+		console.log("gg");
+    return true;
+	} else {
+		console.log("noob");
+    return false;
+	}
+};
+
+isSquare(-1); // false;
+isSquare(0); // true;
+isSquare(3); // false;
+isSquare(4); // true;
+isSquare(25); // true;
+isSquare(26); // false;
+
+*/
