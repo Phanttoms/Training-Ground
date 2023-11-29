0ac4689244d0d720b4d2cf3ec02dff24582c2375
Phanttoms <yohan.pruche@orange.fr>
2023-11-29 17:08:02 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -1,18 +1,4 @@
 /*
-// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
-// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
-
-function fixTheMeerkat(arr) {
-  console.log(arr.reverse());
-  return arr.reverse();
-}
-
-fixTheMeerkat(["tail", "body", "head"])
-fixTheMeerkat(["tails", "body", "heads"])
-fixTheMeerkat(["lower legs", "torso", "upper legs"])
-
-*/
-/*
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
 function moveZeros(arr) {
