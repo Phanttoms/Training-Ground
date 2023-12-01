37505ba9b95b2f898373cb6a6c0bbb1481a39087
Phanttoms <yohan.pruche@orange.fr>
2023-12-01 12:33:41 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -1,24 +1,4 @@
 /*
-// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
-
-function moveZeros(arr) {
-  let result = [];
-  let zero = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      result.push(arr[i]);
-    } else {
-      zero.push(arr[i]);
-    }
-  }
-  console.log(result.concat(zero));
-  return result.concat(zero);
-}
-
-moveZeros([1,2,0,1,0,1,0,3,0,1]) // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
-
-*/
-/*
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
 function pigIt(str){
