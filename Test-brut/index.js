e1b36c07fe014f7ffafe412d505514b9c12e495a
Phanttoms <yohan.pruche@orange.fr>
2023-12-15 21:46:41 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -1,25 +1,4 @@
 /*
-// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
-
-function pigIt(str){
-  str = str.trim().split(/\s{1,}/);
-  result = str.map(val => {
-    if (/^[A-Za-z]+$/.test(val)) {
-      return `${val.slice(1)}${val.slice(0, 1)}ay`;
-    }
-      return val;
-    }).join(' ');
-  
-  console.log(result)
-}
-
-pigIt('Pig latin is cool') // 'igPay atinlay siay oolcay'
-//pigIt('This is my string') // 'hisTay siay ymay tringsay'
-//pigIt('Pig latin is cool') // 'igPay atinlay siay oolcay'
-//pigIt('Hello world !');     // 'elloHay orldway !'
-
-*/
-/*
 // Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
 
 function humanReadable (seconds) {
