073f1ef66746626deaf96b70817b0972b5fbb483
Phanttoms <yohan.pruche@orange.fr>
2024-01-13 02:47:40 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -1,41 +1,4 @@
 /*
-// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
-// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
-// []                                -->  "no one likes this"
-// ["Peter"]                         -->  "Peter likes this"
-// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
-
-function likes(names) {
-	if (names.length === 0) {
-		console.log("no one likes this");
-		return "no one likes this";
-	} else if (names.length === 1) {
-		console.log(`${names} likes this`);
-		return `${names} likes this`;
-	} else if (names.length === 2) {
-		console.log(`${names[0]} and ${names[1]} like this`);
-		return `${names[0]} and ${names[1]} like this`;
-	} else if (names.length === 3) {
-		console.log(`${names[0]}, ${names[1]} and ${names[2]} likes this`);
-		return `${names[0]}, ${names[1]} and ${names[2]} likes this`;
-	} else if (names.length >= 4) {
-		console.log(
-			`${names[0]}, ${names[1]} and ${names.length - 2} others likes this`
-		);
-		return `${names[0]}, ${names[1]} and ${names.length - 2} others likes this`;
-	}
-}
-
-likes([]); // "no one likes this";
-likes(["Peter"]); // "Peter likes this";
-likes(["Jacob", "Alex"]); // "Jacob and Alex like this";
-likes(["Max", "John", "Mark"]); // "Max, John and Mark like this";
-likes(["Alex", "Jacob", "Mark", "Max"]); // "Alex, Jacob and 2 others like this";
-
-*/
-/*
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
 function descendingOrder(n) {
