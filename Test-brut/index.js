22705a97639a17ed0fe969a2ba93b08ca70276ab
Phanttoms <yohan.pruche@orange.fr>
2024-01-19 16:46:38 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -1,34 +1,4 @@
 /*
-// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
-
-function getMiddle(s) {
-	const arr = s.split("");
-	let first = [];
-	let second = [];
-
-	for (let i = 0; i < arr.length; i++) {
-		if (i < arr.length / 2) {
-			first.push(arr[i]);
-		} else {
-			second.push(arr[i]);
-		}
-	}
-	if (arr.length % 2 === 0) {
-		console.log(`${first[first.length - 1]}${second[0]}`);
-		return `${first[first.length - 1]}${second[0]}`;
-	} else {
-		console.log(first[first.length - 1]);
-		return first[first.length - 1];
-	}
-}
-
-getMiddle("test"); // "es"
-getMiddle("testing"); // "t"
-getMiddle("middle"); // "dd"
-getMiddle("A"); // "A"
-
-*/
-/*
 // This time no story, no theory. The examples below show you how to write function accum:
 
 function accum(s) {
