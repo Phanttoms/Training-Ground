751749a33bff80435c849bf09fb91fa09ed98925
Phanttoms <yohan.pruche@orange.fr>
2024-01-29 10:42:59 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -1,17 +1,4 @@
 /*
-
-let online = false;
-
-let message = "";
-
-if (online) {
-	console.log((message = "Utilisateur en ligne !"));
-} else {
-	console.log((message = "Utilisateur hors ligne !"));
-}
-
-*/
-/*
 // You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
 function findOutlier(integers) {
