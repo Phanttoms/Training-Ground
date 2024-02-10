50b7e0013bab08e9382f2f869b86463e0dc6c344
Phanttoms <yohan.pruche@orange.fr>
2024-02-10 14:45:08 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -1,35 +1,4 @@
 /*
-// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
-
-function isIsogram(str) {
-	let array = str.toUpperCase().split("").sort();
-	let result = 0;
-
-	for (let i = 0; i < array.length; i++) {
-		if (array[i] === array[i + 1]) {
-			result++;
-		}
-	}
-
-	if (result === 0) {
-		// console.log(true, result);
-		return true;
-	} else {
-		// console.log(false, result);
-		return false;
-	}
-}
-
-isIsogram("aabbccdd");
-isIsogram("Dermatoglyphics"); // true
-isIsogram("isogram"); // true
-isIsogram("aba"); // false
-isIsogram("moOse"); // false
-isIsogram("isIsogram"); // false
-isIsogram(""); // true
-
-*/
-/*
 // Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
 function XO(str) {
