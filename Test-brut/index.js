f79351432eba05c73e0309ff3c6cd754bf3a6f9c
Phanttoms <yohan.pruche@orange.fr>
2024-03-18 11:44:17 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -30,3 +30,13 @@ XO("Oo"); // false
 XO("ooom"); // false
 
 */
+/*
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+function reverseList(list) {
+	console.log(list.reverse());
+	return list.reverse();
+}
+
+reverseList([1, 2, 3, 4]); // [4, 3, 2, 1];
+reverseList([3, 1, 5, 4]); // [4, 5, 1, 3];
+*/
