311d1202ef6e8edefebb530d36a4787d2f76047f
Phanttoms <yohan.pruche@orange.fr>
2024-03-26 11:36:03 +0100

    kata

--- Test-brut/index.js
+++ Test-brut/index.js
@@ -29,3 +29,23 @@ duplicateCount("aabBcde"); // 2 "should ignore case"
 duplicateCount("Indivisibility"); // 1
 duplicateCount("Indivisibilities"); // 2 "characters may not be adjacent"
 */
+
+//
+
+function canIPlay(now, start, end) {
+	if (start <= now && now < end) {
+		console.log(true);
+		return true;
+	} else if (start > end && (now >= start || now < end)) {
+		console.log(true);
+		return true;
+	} else {
+		console.log(false);
+		return false;
+	}
+}
+
+canIPlay(12, 10, 14); // true "12:00 should match to 10:00-14:00 prime-time"
+canIPlay(12, 13, 14); // false "12:00 shouldn't match to 13:00-14:00 prime-time"
+canIPlay(15, 12, 15); // false "15:00 shouldn't match to 12:00-15:00 prime-time"
+canIPlay(23, 22, 1); // true "23:00 should match to 22:00-01:00 prime-time"
